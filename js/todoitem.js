a3353ed8b3937a7e4106d18ace0fb38b29c3e70d
Jared Hanson <jaredhanson@gmail.com>
2022-05-09 09:47:07 -0700

    Add like button from React starter.

---
+++ js/todoitem.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    console.log('#render');
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+console.log('redering like button');
+root.render(e(LikeButton));
