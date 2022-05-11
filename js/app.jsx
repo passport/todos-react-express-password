353ed6a5453dc6da504f469b001a1516e5970467
Jared Hanson <jaredhanson@gmail.com>
2022-05-11 15:50:44 -0700

    Clean up.

--- js/app.jsx
+++ js/app.jsx
@@ -81,29 +81,6 @@ class App extends React.Component {
         </footer>
       </AuthProvider>
       </>
-    )
-    
-    
-    
-    var user = this.props.user;
-    if (!user) {
-      return <Welcome />
-      //return <LoginPrompt />
-    }
-    
-    return (
-      <div>
-        <Header />
-        <section className="main">
-          <input id="toggle-all" className="toggle-all" type="checkbox" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-            <TodoItem value="Taste JavaScript" />
-            <TodoItem value="Buy a unicorn" />
-          </ul>
-        </section>
-        <Footer />
-      </div>
     );
   }
 }
