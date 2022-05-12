71fc5db1819305c4f420767c4ae21618bd24a088
Jared Hanson <jaredhanson@gmail.com>
2022-05-11 17:01:57 -0700

    Setup local strategy.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
+var crypto = require('crypto');
+var db = require('../db');
+
+
+passport.use(new LocalStrategy((username, password, cb) => {
+  db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+    
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+      return cb(null, row);
+    });
+  });
+}));
+
+passport.serializeUser((user, cb) => {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username });
+  });
+});
+
+passport.deserializeUser((user, cb) => {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
 
 
 var router = express.Router();
 
-router.post('/login/password', (req, res) => {
+router.post('/login/password', passport.authenticate('local', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login',
+  failureMessage: true
+}), (req, res) => {
   console.log('TODO: login password');
   console.log(req.body);
 });
