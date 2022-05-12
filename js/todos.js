fe1c9dbe01596c4cd52b7e5efc9cbc9f7e39f603
Jared Hanson <jaredhanson@gmail.com>
2022-05-11 18:21:00 -0700

    Add account status nav to app.

--- js/todos.js
+++ js/todos.js
@@ -18,6 +18,16 @@ function Todos() {
   
   return (
     <section className="todoapp">
+      <nav className="nav">
+        <ul>
+          <li className="user">{auth.user.username}</li>
+          <li>
+            <form action="/logout" method="post">
+              <button className="logout" type="submit">Sign out</button>
+            </form>
+          </li>
+        </ul>
+      </nav>
       <Header />
       <section className="main">
         <input id="toggle-all" className="toggle-all" type="checkbox" />
