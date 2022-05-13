415eed9a4487735015f5e24d6830dcfe404932c0
Jared Hanson <jaredhanson@gmail.com>
2022-05-13 06:56:02 -0700

    WIP: editing todo items.

--- js/todoitem.js
+++ js/todoitem.js
@@ -1,15 +1,38 @@
 'use strict';
 
 function TodoItem({ value, editing, onToggle, onEdit }) {
+  // WIP: add editing state here and finish implementing editing
+  
+  const [editedTitle, setEditedTitle] = React.useState(value.title);
+  
+  const ENTER_KEY = 13;
+  const ESCAPE_KEY = 27;
   
   const handleToggle = (event) => {
     value.completed = !value.completed;
     onToggle(value);
   };
   
-  const handleEdit = (event) => {
+  const handleDoubleClick = (event) => {
     console.log('editing...')
     onEdit(value);
+  };
+  
+  const handleKeyDown = (event) => {
+    console.log('key down');
+    console.log(event.keyCode);
+    console.log(event.which)
+    
+    /*
+    if (event.keyCode !== ENTER_KEY) { return; }
+    var value = event.target.value.trim();
+    if (!value) { return; }
+    onSubmit();
+    */
+  };
+  
+  const handleBlur = (event) => {
+    console.log('submit...')
   };
   
   const handleClick = async (event) => {
@@ -25,10 +48,14 @@ function TodoItem({ value, editing, onToggle, onEdit }) {
       {!editing
         ? <div className="view">
             <input className="toggle" type="checkbox" checked={value.completed ? true : false} onChange={handleToggle} />
-            <label onDoubleClick={handleEdit}>{value.title}</label>
+            <label onDoubleClick={handleDoubleClick}>{value.title}</label>
             <button className="destroy" onClick={handleClick}></button>
           </div>
-        : <input className="edit" defaultValue={value.title} />
+        : <input className="edit" value={editedTitle}
+            onChange={event => { setEditedTitle(event.target.value); }}
+            onKeyDown={handleKeyDown}
+            onBlur={handleBlur}
+          />
       }
     </li>
   );
